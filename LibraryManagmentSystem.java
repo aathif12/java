129ddd0c97358d3433c3faea65779598fb184389
Aathif Ahamed <55709375+aathif12@users.noreply.github.com>
2023-07-15 20:42:36 +0530

    Add files via upload

---
+++ LibraryManagmentSystem.java
@@ -0,0 +1,141 @@
+
+abstract class LibraryItem {
+    private String title;
+    private String itemID;
+    private boolean checkedOut;
+
+    public LibraryItem(String title, String itemID) {
+        this.title = title;
+        this.itemID = itemID;
+        this.checkedOut = false;
+    }
+
+    public void checkOut() {
+        if (!checkedOut) {
+            checkedOut = true;
+            System.out.println(title + " checked out.");
+        } else {
+            System.out.println(title + " is already checked out.");
+        }
+    }
+
+    public void checkIn() {
+        if (checkedOut) {
+            checkedOut = false;
+            System.out.println(title + " checked in.");
+        } else {
+            System.out.println(title + " is already checked in.");
+        }
+    }
+
+    public void displayItemDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Item ID: " + itemID);
+        System.out.println("Checked Out: " + checkedOut);
+    }
+}
+
+
+class Book extends LibraryItem {
+    private String author;
+    private int numPages;
+
+    public Book(String title, String itemID, String author, int numPages) {
+        super(title, itemID);
+        this.author = author;
+        this.numPages = numPages;
+    }
+
+    public void displayItemDetails() {
+        super.displayItemDetails();
+        System.out.println("Author: " + author);
+        System.out.println("Number of Pages: " + numPages);
+    }
+}
+
+
+class Magazine extends LibraryItem {
+    private String issueDate;
+    private String publisher;
+
+    public Magazine(String title, String itemID, String issueDate, String publisher) {
+        super(title, itemID);
+        this.issueDate = issueDate;
+        this.publisher = publisher;
+    }
+
+    public void displayItemDetails() {
+        super.displayItemDetails();
+        System.out.println("Issue Date: " + issueDate);
+        System.out.println("Publisher: " + publisher);
+    }
+}
+
+
+class LibraryMember {
+    private String memberID;
+    private String name;
+
+    public LibraryMember(String memberID, String name) {
+        this.memberID = memberID;
+        this.name = name;
+    }
+
+    public void displayMemberDetails() {
+        System.out.println("Member ID: " + memberID);
+        System.out.println("Name: " + name);
+    }
+}
+
+class LibraryManagementSystem {
+    public static void main(String[] args) {
+      
+        Book book1 = new Book("The Catcher in the Rye", "B001", "J.D. Salinger", 240);
+        Book book2 = new Book("To Kill a Mockingbird", "B002", "Harper Lee", 281);
+        Book book3 = new Book("1984", "B003", "George Orwell", 328);
+        Book book4 = new Book("Pride and Prejudice", "B004", "Jane Austen", 432);
+        Book book5 = new Book("The Hobbit", "B005", "J.R.R. Tolkien", 320);
+
+        
+        Magazine magazine1 = new Magazine("National Geographic", "M001", "August 2023", "National Geographic Society");
+        Magazine magazine2 = new Magazine("Time", "M002", "September 2023", "Time USA, LLC");
+        Magazine magazine3 = new Magazine("Forbes", "M003", "June 2023", "Forbes Media");
+        Magazine magazine4 = new Magazine("Vogue", "M004", "July 2023", "Condé Nast");
+        Magazine magazine5 = new Magazine("Sports Illustrated", "M005", "July 2023", "Maven Coalition");
+
+       
+        LibraryMember member1 = new LibraryMember("M001", "John Doe");
+        LibraryMember member2 = new LibraryMember("M002", "Jane Smith");
+        LibraryMember member3 = new LibraryMember("M003", "Robert Johnson");
+        LibraryMember member4 = new LibraryMember("M004", "Emily Davis");
+        LibraryMember member5 = new LibraryMember("M005", "Michael Wilson");
+
+      
+
+       
+        book1.checkOut();
+        book1.checkIn();
+
+
+        System.out.println("Books:");
+        book1.displayItemDetails();
+        book2.displayItemDetails();
+        book3.displayItemDetails();
+        book4.displayItemDetails();
+        book5.displayItemDetails();
+
+        System.out.println("\nMagazines:");
+        magazine1.displayItemDetails();
+        magazine2.displayItemDetails();
+        magazine3.displayItemDetails();
+        magazine4.displayItemDetails();
+        magazine5.displayItemDetails();
+
+        System.out.println("\nLibrary Members:");
+        member1.displayMemberDetails();
+        member2.displayMemberDetails();
+        member3.displayMemberDetails();
+        member4.displayMemberDetails();
+        member5.displayMemberDetails();
+    }
+}
